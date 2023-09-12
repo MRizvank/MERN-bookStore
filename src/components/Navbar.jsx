4130da7e6de59896a5b514e6a533be3d6219e6e4
Rizvan <rizvankurungattil@gmail.com>
2023-09-12 23:32:17 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+// import { useState } from "react";
+const Navbar = () => {
+  //   const [isDark, setDark] = useState("");
+  const handleDark = () => {
+    // console.log();
+  };
+  return (
+    <div>
+      <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+        <div>
+          <span className="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
+            hi
+          </span>
+        </div>
+        <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">
+          Writes Upside-Down
+        </h3>
+        <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+          The Zero Gravity Pen can be used to write in any orientation,
+          including upside-down. It even works in outer space.
+        </p>
+      </div>
+      <button onClick={handleDark}>dark</button>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { SnackbarProvider, enqueueSnackbar } from "notistack";
+import { SnackbarProvider } from "notistack";
+import Navbar from "./components/Navbar";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <SnackbarProvider>
       <App />
+      <Navbar />
     </SnackbarProvider>
   </BrowserRouter>
 );
